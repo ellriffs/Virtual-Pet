0c54b505c51dc64bdef2ce4d8d6abf238a8714bc
Elliot Robertson <elliotrobertson53@gmail.com>
2021-05-12 23:35:11 +0100

    added walk function and fitness levels

--- _tests_/pet.test.js
+++ _tests_/pet.test.js
@@ -1,11 +1,14 @@
 const Pet = require('../src/pet');
 
+// Name Fucntion//
 describe('constructor', () => {
     it('sets the name property', () => {
     const pet = new Pet('Fido');
     expect(pet.name).toEqual('Fido');
     });
 });
+
+//Age test//
 
 describe('constructor', () => {
 
@@ -16,10 +19,48 @@ describe('constructor', () => {
     });
 });
 
+//Grow Up function Test//
+
 describe('growUp', () => {
     it('increments the age by 1', () => {
     const pet = new Pet('Fido');
     pet.growUp();
     expect(pet.age).toEqual(1);
+    expect(pet.hunger).toEqual(5);
+    expect(pet.fitness).toEqual(7)
+    });
+});
+
+// Walk Function //
+
+describe('walk',() => {
+    it('increases fitness level by 4', () => {
+    const pet = new Pet('Fido');
+    pet.fitness = 0;
+    pet.walk();
+    expect(pet.fitness).toEqual(4)
+    
+    });
+});
+
+//Hunger Test//
+
+describe('constructor', () => {
+
+    it('has a initial hunger of 0', () => {
+    const pet = new Pet('Fido');
+
+    expect(pet.hunger).toEqual(0);
+    });
+});
+
+// Fitness Test //
+
+describe('constructor', () => {
+
+    it('has a initial fitness of 10', () => {
+    const pet = new Pet('Fido');
+
+    expect(pet.fitness).toEqual(10);
     });
 });
--- src/pet.js
+++ src/pet.js
@@ -1,10 +1,27 @@
+const maxFitness = 10;
 
 function Pet(name) {
     this.name = name;
     this.age = 0;
+    this.hunger = 0;
+    this.fitness = 10;
 }
     Pet.prototype.growUp = function(){
-    this.age += 1
+    this.age += 1, this.hunger += 5, this.fitness -= 3
+}
+    Pet.prototype.walk = function(){
+    if (this.fitness === 10){
+        this.fitness    
+    } if (this.fitness === 9) {
+        this.fitness += 1
+    } if (this.fitness === 8){
+        this.fitness += 2
+    } if (this.fitness === 7){
+        this.fitness += 3
+    }if (this.fitness <=6){
+        this.fitness += 4
+    };
+
     };
 
 module.exports = Pet;
