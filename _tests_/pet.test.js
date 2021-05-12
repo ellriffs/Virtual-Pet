7fd807d3532de1bb1469ea63f3ee2673b8ed395c
Elliot Robertson <elliotrobertson53@gmail.com>
2021-05-12 22:22:45 +0100

    added prototype. full CLI functionality

--- _tests_/pet.test.js
+++ _tests_/pet.test.js
@@ -1,7 +1,25 @@
 const Pet = require('../src/pet');
 
 describe('constructor', () => {
-    it('returns an object', () => {
-    expect(new Pet('Fido')).toBeInstanceOf(Object);
+    it('sets the name property', () => {
+    const pet = new Pet('Fido');
+    expect(pet.name).toEqual('Fido');
+    });
+});
+
+describe('constructor', () => {
+
+    it('has a initial age of 0', () => {
+    const pet = new Pet('Fido');
+
+    expect(pet.age).toEqual(0);
+    });
+});
+
+describe('growUp', () => {
+    it('increments the age by 1', () => {
+    const pet = new Pet('Fido');
+    pet.growUp();
+    expect(pet.age).toEqual(1);
     });
 });
--- src/pet.js
+++ src/pet.js
@@ -1,5 +1,11 @@
 
-function Pet() {}
+function Pet(name) {
+    this.name = name;
+    this.age = 0;
+}
+    Pet.prototype.growUp = function(){
+    this.age += 1
+    };
 
 module.exports = Pet;
 
